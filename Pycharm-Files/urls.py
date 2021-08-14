ecb264325ec74498876f845421a2bbe2bd729773
SK <itzmesk2000@gmail.com>
2021-08-14 09:08:46 +0530

    Add files via upload

--- Pycharm-Files/urls.py
+++ Pycharm-Files/urls.py
@@ -1,21 +1,23 @@
+"""hospital_mngt URL Configuration
 
-from django.urls import path
-from .views import About, Home , Contact, Login, Logout_admin, Index, View_Doctor,Delete_Doctor,Add_Doctor ,View_Patient ,Add_Patient,Delete_Patient,Add_Appointment,View_Appointment,Delete_Appointment
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+
 
 urlpatterns = [
-    path('', Home, name='home'),
-    path('about/', About, name='about'),
-    path('contact/', Contact, name='contact'),
-    path('admin_login/', Login, name='admin_login'),
-    path('logout/', Logout_admin, name='logout_admin'),
-    path('index/', Index, name='dashboard'),
-    path('view_doctor/', View_Doctor, name='view_doctor'),
-    path('view_patient/', View_Patient, name='view_patient'),
-    path('view_appointment/',View_Appointment,name='view_appointment'),
-    path('add_doctor/', Add_Doctor, name='add_doctor'),
-    path('add_patient/' ,Add_Patient, name='add_patient'),
-    path('add_appointment',Add_Appointment,name='add_appointment'),
-    path('delete_doctor(?P<int:pid>)' , Delete_Doctor , name='delete_doctor'),
-    path('delete_patient(?P<int:pid>)',Delete_Patient, name='delete_patient'),
-    path('delete_Appointment(?P<int:pid>)',Delete_Appointment, name='delete_appointment'),
+    path('admin/', admin.site.urls),
+    path('',include('hospital.urls')),
 ]
